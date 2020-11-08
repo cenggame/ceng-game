0d184518047331164d8eb0a4a3f3ae49797ccd36
sametmert03 <samet.mert03@live.com>
2020-11-08 19:09:01 +0300

    menu

---
+++ Iso/Assets/Scripts/UIMain.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIMain : MonoBehaviour
+{
+    [Header("UIPages")]
+    public GameObject settingsScreen;
+    public GameObject creditsScreen;
+    public GameObject mainScreen;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Settings()
+    {
+        mainScreen.SetActive(false);
+        settingsScreen.SetActive(true);
+        
+    }
+
+    public void SetToMenu()
+    {
+        mainScreen.SetActive(true);
+        settingsScreen.SetActive(false);
+    }
+    public void Credits()
+    {
+        mainScreen.SetActive(false);
+        creditsScreen.SetActive(true);
+    }
+
+    public void CreToMenu()
+    {
+        mainScreen.SetActive(true);
+        creditsScreen.SetActive(false);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
