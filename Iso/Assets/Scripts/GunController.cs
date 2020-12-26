c8f12408151172e8f67261f43bba013f00b0c786
wannaberoot <kamilozansahin@eskisehir.edu.tr>
2020-12-27 02:50:55 +0300

    Reload system added

--- Iso/Assets/Scripts/GunController.cs
+++ Iso/Assets/Scripts/GunController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GunController : MonoBehaviour
 {
@@ -11,16 +12,32 @@ public class GunController : MonoBehaviour
     public float shotCounter;
     public Transform firePoint;
     private AudioSource mAudioSrc;
+    public int maxAmmo = 10;
+    public int currentAmmo;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
+    public Text ammoText;
 
     // Start is called before the first frame update
     void Start()
     {
         mAudioSrc = GetComponent<AudioSource>();
+        currentAmmo = maxAmmo;
+        showAmmo();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isReloading)
+        {
+            return;
+        }
+        if (currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
         if (isFiring)
         {
             shot();
@@ -30,11 +47,29 @@ public class GunController : MonoBehaviour
             shotCounter = 0;
         }
     }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        yield return new WaitForSeconds(reloadTime);
+        currentAmmo = maxAmmo;
+        showAmmo();
+        isReloading = false;
+    }
+
+    void showAmmo()
+    {
+        ammoText = GameObject.Find("Ammo").GetComponent<Text>();
+        ammoText.text = currentAmmo + "/" + maxAmmo;
+    }
+
     void shot()
     {
         shotCounter -= Time.deltaTime;
         if (shotCounter <= 0)
         {
+            currentAmmo--;
+            showAmmo();
             shotCounter = timeBetweenShots;
             BulletController newBullet = Instantiate(bullet, firePoint.position, firePoint.rotation) as BulletController;
             newBullet.speed = bulletSpeed;
