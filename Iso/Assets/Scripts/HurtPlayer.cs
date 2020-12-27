0e2926f00f0b093e195c9074bbf9f254dd4f59d4
sametmert03 <samet.mert03@live.com>
2020-12-27 21:42:57 +0300

    Death Animation, Sounds
    Enemies have death animation, fixing damage to take while enemy is dead.
    
    Adding sound of zombies, ambient sound and walking sound

--- Iso/Assets/Scripts/HurtPlayer.cs
+++ Iso/Assets/Scripts/HurtPlayer.cs
@@ -5,19 +5,32 @@ using UnityEngine;
 
 public class HurtPlayer : MonoBehaviour
 {
+    //public Animator _anim;
     public int damageToGive;
     public float cooldownTime = 1;
     public float nextFireTime = 0;
 
-
+    private void Start()
+    {
+        //_anim = GetComponent<Animator>();
+    }
     void OnTriggerStay(Collider other)
     {
+       // isDead = GetComponent<EnemyHealthManager>().isDead;
         if (Time.time >nextFireTime)
         {
-            other.gameObject.GetComponent<PlayerHealthManager>().HurtPlayer(damageToGive);
-            nextFireTime = Time.time + cooldownTime;
+            if (damageToGive != 0)
+            {
+               // gameObject.GetComponent<EnemyController>().attack();
+                other.gameObject.GetComponent<PlayerHealthManager>().HurtPlayer(damageToGive);
+                nextFireTime = Time.time + cooldownTime;
+            }
+            
         }
     }
     
+    
+
+
 }
 
--- Iso/Assets/charCont.cs
+++ Iso/Assets/charCont.cs
@@ -12,6 +12,9 @@ public class charCont : MonoBehaviour
     Animator chr_anim;
     private Camera mainCamera;
     public GunController theGun;
+    AudioSource au;
+    public float cooldownTime = 0.001f;
+    public float nextFireTime = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -23,8 +26,8 @@ public class charCont : MonoBehaviour
         forward = Vector3.Normalize(forward);
         right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
         theGun.isFiring = false;
+        au = GetComponent<AudioSource>();
 
-            
             
             }
 
@@ -60,6 +63,11 @@ public class charCont : MonoBehaviour
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
        {
             Move();
+            if (Time.time > nextFireTime)
+            {
+                au.Play();
+                nextFireTime = Time.time + cooldownTime;
+            }
        }
         
         
