68f4df9d216d0b3c6c9f134bcb03090d2bf75314
sametmert03 <samet.mert03@live.com>
2020-12-29 21:58:24 +0300

    score system update

--- Iso/Assets/Scripts/EnemyHealthManager.cs
+++ Iso/Assets/Scripts/EnemyHealthManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class EnemyHealthManager : MonoBehaviour
 {
@@ -12,7 +13,15 @@ public class EnemyHealthManager : MonoBehaviour
     private int currentHealth;
     public bool isDead = false;
     public int damage = 2;
-    
+    PlayerHealthManager crHealth;
+
+
+
+    public static  int score;
+    int scoreValue = 10;
+    public int gainedHealth = 5;
+    public Text scoreText;
+
     void Start()
     {
         capsule = GetComponent<CapsuleCollider>();
@@ -20,20 +29,25 @@ public class EnemyHealthManager : MonoBehaviour
         currentHealth = health;
         mAudioSrc.Play();
         damage = 2;
+        score = 0;
     }
     void Update()
     {
+        scoreText.text = "Score: " + score.ToString();
+
         if (currentHealth <= 0)
         {
+            
             if (!isDead)
             {
+                
+                ScoreUpdate();
                 gameObject.GetComponent<EnemyController>().die();
                 isDead = true;
                 mAudioSrc.Stop();
                 capsule.enabled = false;
                 damage = 0;
                 hurtZone.SetActive(false);
-                //ScoreCounter.updateScore(); 
             }
             else
                 damage = 2;
@@ -42,5 +56,16 @@ public class EnemyHealthManager : MonoBehaviour
     public void HurtEnemy(int damage)
     {
         currentHealth -= damage;
+    }
+
+    public void ScoreUpdate()
+    {
+        score += scoreValue;
+
+        //If score is multiple of 100, player will gain some health 
+        if (score != 0 && score % 100 == 0)
+        {
+           // crHealth.currentHealth += gainedHealth;
+        }
     }
 }
