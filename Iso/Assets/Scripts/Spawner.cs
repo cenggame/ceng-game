68d11ad2ac94b6c2807a8a9520319653edd3b006
wannaberoot <kamilozansahin@eskisehir.edu.tr>
2021-01-05 22:59:09 +0300

    Starting Text Added

--- Iso/Assets/Scripts/Spawner.cs
+++ Iso/Assets/Scripts/Spawner.cs
@@ -11,7 +11,8 @@ public class Spawner : MonoBehaviour
     public GameObject[] spawners;
     public GameObject enemy;
     int scc;
-    bool isWaiting=false;
+    bool isWaiting = false;
+    bool isStarting = true;
     public Text waveStatus;
 
 
@@ -28,20 +29,28 @@ public class Spawner : MonoBehaviour
         {
             spawners[i] = transform.GetChild(i).gameObject;
         }
-            StartWave();
 
-        
-        waveStatus = GameObject.Find("Wave Status").GetComponentInChildren<Text>();
-        waveStatus.text = "Wave  " + waveNumber;
+        if(isStarting)
+        {
+            StartCoroutine(Starting());
+        }
+        else
+        {
+            StartWave();
+        }
     }
 
     private void Update()
     {
 
-         if ( (EnemyHealthManager.score/10) >= (enemySpawnAmount + (scc) ) )
+         if ((EnemyHealthManager.score/10) >= (enemySpawnAmount + (scc)))
         {
             
             if(isWaiting)
+            {
+                return;
+            }
+            else if(isStarting)
             {
                 return;
             }
@@ -63,6 +72,8 @@ public class Spawner : MonoBehaviour
     public void StartWave()
     {
         waveNumber = 1;
+        waveStatus = GameObject.Find("Wave Status").GetComponentInChildren<Text>();
+        waveStatus.text = "Wave  " + waveNumber;
         enemySpawnAmount = 2;
         enemiesKilled = 0;
 
@@ -75,19 +86,31 @@ public class Spawner : MonoBehaviour
      IEnumerator NextWave()
     {
         waveStatus.text = "Get ready for the next wave!";
-            isWaiting = true;
-            scc = EnemyHealthManager.score / 10;
-            waveNumber++;
-            yield return new WaitForSeconds(5f);
-            enemySpawnAmount += 2;
-            enemiesKilled = 0;
-            for (int i = 0; i < enemySpawnAmount; i++)
+        isWaiting = true;
+        scc = EnemyHealthManager.score / 10;
+        waveNumber++;
+        yield return new WaitForSeconds(5f);
+        enemySpawnAmount += 2;
+        enemiesKilled = 0;
+        for (int i = 0; i < enemySpawnAmount; i++)
             {
                 SpawnEnemy();
             }
-            waveStatus.text = "Wave : "+waveNumber;
+        waveStatus.text = "Wave : "+waveNumber;
 
         isWaiting = false;
+    }
+
+    IEnumerator Starting()
+    {
+        waveStatus.text = "Get Ready!";
+        yield return new WaitForSeconds(2f);
+        waveStatus.text = "Equip Your Weapon!";
+        yield return new WaitForSeconds(2f);
+        waveStatus.text = "Save Your Campus!";
+        yield return new WaitForSeconds(2f);
+
+        isStarting = false;
     }
 
   
